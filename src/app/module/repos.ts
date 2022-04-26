d88f80f1e396f1b4868cb16709c878a0f1322a2b
mccafee025 <ademaamanka@gmail.com>
2022-04-26 16:37:44 +0300

    create repo class and user class

---
+++ src/app/module/repos.ts
@@ -0,0 +1,9 @@
+export class Repos {
+      constructor (
+            public name:any,
+            public description:any,
+            public watchers:any,
+            public language:any,
+            public forks:any
+      ){}
+}
---
+++ src/app/module/users.ts
@@ -0,0 +1,10 @@
+export class Users {
+      constructor(
+            public avatar_url:any,
+            public name:any,
+            public bio:any,
+            public followers:any,
+            public following:any,
+            public public_repos:any
+      ){}
+}
