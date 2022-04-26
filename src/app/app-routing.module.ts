9ec8b26fc809f329888b4caebc83ef47ab5e7528
mccafee025 <ademaamanka@gmail.com>
2022-04-26 17:13:02 +0300

    import components and specify paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserRepoComponent } from './user-repo/user-repo.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+
+
+const routes: Routes = [
+  { path: '', component: UserDataComponent },
+  { path: 'user-repo', component: UserRepoComponent },
+  { path: '**', component: NotFoundComponent },
+
+  // Empty Paths
+  { path: '', redirectTo: "**", pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
